3b81bf2d02aafbc2ac1940faea2698d20bebfd63
vivekmuniyandi <vivek.muniyandi@asu.edu>
2017-04-15 15:06:30 -0700

    Implementation without geospark - initial

--- src/main/scala/edu/geospark/frcompute/GetHotSpotswithoutGspark.scala
+++ src/main/scala/edu/geospark/frcompute/GetHotSpotswithoutGspark.scala
@@ -25,109 +25,104 @@ import java.lang._
 import collection.JavaConverters._
 import scala.collection.Map
 
-object GetHotSpots 
+object GetHotSpotswithoutGspark
 {
   def loadCSV() : RDD[Row] = 
   {
     val spark = SparkSession.builder().appName("Load CSV").getOrCreate()
-    // val timeStampDF = spark.read.option("header", "true").csv("/Users/Vivek/Studies/MS/DDS/Phases/3/Dataset/sample-code.csv")
-    val timeStampDF = spark.read.option("header", "true").csv("hdfs://master:54310/user/hduser/dataset/yellow_tripdata_2015-01.csv")
+    val timeStampDF = spark.read.option("header", "true").csv("/Users/Vivek/Studies/MS/DDS/Phases/3/Dataset/yellow_tripdata_2015-01.csv")
+    //val timeStampDF = spark.read.option("header", "true").csv("hdfs://master:54310/user/hduser/dataset/yellow_tripdata_2015-01.csv")
     timeStampDF.select("tpep_pickup_datetime", "pickup_longitude", "pickup_latitude").rdd
   }
   
-  def getRectangleRDD (boundaryEnvelope : Envelope, geometryFactory : GeometryFactory) : (ArrayList[Polygon], ArrayList[(Double, Double)]) =
+  def getAllSquares (boundaryEnvelope : Envelope) : ArrayList[(Double, Double)] =
   {
     // val boundaryEnvelope = new Envelope(0,10,0,10)
     val intervalX = 0.01
 		val intervalY = 0.01
 		val horizontalPartitions = Math.ceil((boundaryEnvelope.getMaxX - boundaryEnvelope.getMinX) / intervalX).toInt
 		val verticalPartitions = Math.ceil((boundaryEnvelope.getMaxY - boundaryEnvelope.getMinY) / intervalY).toInt
-		val polygons = new ArrayList[Polygon]
     val squares = new ArrayList[(Double, Double)]
 		for (i <- 0 to (horizontalPartitions - 1))
 		{
 		  for (j <- 0 to (verticalPartitions - 1))
 		  {
 		    var x1 = boundaryEnvelope.getMinX()+intervalX*i
-		    var x2 = boundaryEnvelope.getMinX()+intervalX*(i+1)
 		    var y1 = boundaryEnvelope.getMinY()+intervalY*j
-		    var y2 = boundaryEnvelope.getMinY()+intervalY*(j+1)
-		    var coordinates = new Array[Coordinate](5);
-        coordinates(0) = new Coordinate(x1,y1);
-        coordinates(1) = new Coordinate(x1,y2);
-        coordinates(2) = new Coordinate(x2,y2);
-        coordinates(3) = new Coordinate(x2,y1);
-        coordinates(4) = coordinates(0);
-		    var polygon = new Polygon(geometryFactory.createLinearRing(coordinates), null, geometryFactory)
-		    polygons.add(polygon);
-		    squares.add((x1, y1))
+		    val roundednewx = BigDecimal(x1).setScale(2, BigDecimal.RoundingMode.HALF_UP).toDouble
+        val roundednewy = BigDecimal(y1).setScale(2, BigDecimal.RoundingMode.HALF_UP).toDouble
+		    squares.add((roundednewx, roundednewy))
 		  }
 		}
-		(polygons, squares)
+		squares
   }
  
   def main(args: Array[String]): Unit = 
   {
     Logger.getLogger("org").setLevel(Level.OFF)
     Logger.getLogger("akka").setLevel(Level.OFF)
-    val conf = new SparkConf().setAppName("Get Hotspots").setMaster("spark://192.168.0.200:7077").set("spark.driver.host","192.168.0.200").set("spark.ui.port","4040")
-    // val conf = new SparkConf().setAppName("Get Hotspots").setMaster("local")
+    //val conf = new SparkConf().setAppName("Get Hotspots").setMaster("spark://192.168.0.200:7077").set("spark.driver.host","192.168.0.200").set("spark.ui.port","4040")
+    val conf = new SparkConf().setAppName("Get Hotspots").setMaster("local")
     val sc = new SparkContext(conf)
-    val geometryFactory = new GeometryFactory();
-    var pointsCountRDD : Map[(scala.Double, scala.Double, scala.Int), scala.Long] = Map()
+    val interval = 0.01
+    
+    // Construct the squares for the new york envelope
+    val boundaryEnvelope = new Envelope(-74.25, -73.7, 40.5, 40.9)  
+    val minX = boundaryEnvelope.getMinX
+    val maxX = boundaryEnvelope.getMaxX
+    val minY = boundaryEnvelope.getMinY
+    val maxY = boundaryEnvelope.getMaxY
+    val minXBC = sc.broadcast(minX)
+    val maxXBC = sc.broadcast(maxX)
+    val minYBC = sc.broadcast(minY)
+    val maxYBC = sc.broadcast(maxY)
+    
+    val rectangleGrids = getAllSquares(boundaryEnvelope)
+    val intervalBC = sc.broadcast(interval)
     val timeStampRDD = loadCSV
     val format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
     var cal = Calendar.getInstance()
     
-    // Get the pointRDD containing all the points
-    val dayRDD = timeStampRDD.map 
-                 { x => 
-                    cal.setTime(format.parse(x.get(0).toString))
-                    (cal.get(Calendar.DAY_OF_MONTH), geometryFactory.createPoint(new Coordinate(Double.parseDouble(x.get(1).toString()), Double.parseDouble(x.get(2).toString()))))
-                 }.cache()
-    val pointRDD = new PointRDD(dayRDD.map(x => x._2))
-    
-    // Filter the RDD to contain points only within the New york envelope
-    val newyorkEnvelope = new Envelope(-74.25, -73.7, 40.5, 40.9)
-    pointRDD.buildIndex(IndexType.RTREE, false)
-    val filteredPointRDD = new PointRDD(RangeQuery.SpatialRangeQuery(pointRDD, newyorkEnvelope, true, true))
-    println("Filtered points " +filteredPointRDD.rawSpatialRDD.count())
-    
-    // Get the rectangle RDD
-    val boundaryEnvelope = newyorkEnvelope
-    val minX = boundaryEnvelope.getMinX
-    val minY = boundaryEnvelope.getMinY
-    val maxX = boundaryEnvelope.getMaxX
-    val maxY = boundaryEnvelope.getMaxY
-    val rectangleGrids = getRectangleRDD(boundaryEnvelope, geometryFactory)
-    val rectangleRDD = new RectangleRDD(new JavaRDD(sc.parallelize(rectangleGrids._1.asScala)))
-    
-    // Do the spatial join in order to build each cube's attribute values
-    var joinResults = sc.emptyRDD[((scala.Double, scala.Double, scala.Int), scala.Long)]
-    for (i <- 1 to 31)
-    {
-      val pointDayRdd = new PointRDD(dayRDD
-                                        .filter(x => x._1 == i)
-                                        .map(x => x._2))
-      pointDayRdd.spatialPartitioning(GridType.RTREE);
-      rectangleRDD.spatialPartitioning(pointDayRdd.grids);
-      val tempJoinRDD = JoinQuery.SpatialJoinQueryCountByKey(pointDayRdd, rectangleRDD, false, true).rdd
-                                    .map(x => ((x._1.getCoordinates()(0).x, x._1.getCoordinates()(0).y, i), x._2.toLong))
-      println("Temp RDD : " + i + " "  + tempJoinRDD.count())
-      println(tempJoinRDD.map(x => x._2).reduce((x, y) => x+ y))
-      joinResults = joinResults.union(tempJoinRDD)
-      println("Join Result RDD : " + joinResults.count())
-    }
-    dayRDD.unpersist();
-    pointsCountRDD = joinResults.collectAsMap()
+    // Get the pointRDD containing all the points which fall within the Newyork envelope
+    val dayFilteredRDD = timeStampRDD.map 
+                         { x => 
+                            cal.setTime(format.parse(x.get(0).toString))
+                            (x.get(1).toString().toDouble, x.get(2).toString().toDouble, cal.get(Calendar.DAY_OF_MONTH))
+                         }
+                         .filter(x => x._1 >= minXBC.value && x._1 <= maxXBC.value && x._2 >= minYBC.value && x._2 <= maxYBC.value)
+                         .cache()
+    // dayFilteredRDD.foreach(x => println(x))
+    // println(dayFilteredRDD.count())
+   
+    // Function to find the cell's X and Y value for a given point
+    def findCell(x : (Double, Double)) : (Double, Double) = 
+                         {
+                             val newx = (Math.floor((x._1 / intervalBC.value)) * intervalBC.value)
+                             val newy = (Math.floor((x._2 / intervalBC.value)) * intervalBC.value)
+                             val roundednewx = BigDecimal(newx).setScale(2, BigDecimal.RoundingMode.HALF_UP).toDouble
+                             val roundednewy = BigDecimal(newy).setScale(2, BigDecimal.RoundingMode.HALF_UP).toDouble
+                             (roundednewx, roundednewy)
+                         }
+   // println(findCell(-74.0075759887695,40.7325363159179))
+   // println(findCell(-74.0163955688476,40.7064018249511))
+    // Construct the map for the cell value
+    val cubeAttributeRDD = dayFilteredRDD.map
+                           {
+                             x => 
+                             val cellValue = findCell(x._1, x._2)
+                             ((cellValue._1, cellValue._2, x._3), 1.toLong)
+                           }
+                           .reduceByKey((x, y) => x + y)
+       
+    //cubeAttributeRDD.takeOrdered(50)(Ordering[Long].reverse.on { x => x._2 }).foreach(x => println(x))
+    // println(cubeAttributeRDD.map(x => x._2).reduce((x, y) => x + y))
     // Variables for calculating the neighbour values
-    val pointsCountMapBC = sc.broadcast(pointsCountRDD)
-    val minXBC = sc.broadcast(minX)
-    val minYBC = sc.broadcast(minY)
-    val maxXBC = sc.broadcast(maxX)
-    val maxYBC = sc.broadcast(maxY)
+    val cubeAttributeMap = cubeAttributeRDD.collectAsMap()
+    val pointsCountMapBC = sc.broadcast(cubeAttributeMap)
     val xArrayBC = sc.broadcast(Array(0, 0.01, -0.01, 0, 0, -0.01, 0.01, 0.01, -0.01))
     val yArrayBC = sc.broadcast(Array(0, 0, 0, 0.01, -0.01, 0.01, -0.01, 0.01, -0.01))
+    
+    // Function to get the neighbour values including itself and the number of neighbours
     def getNeighbourValues(x : Double, y : Double, day : Int) : (Long, Int) =
     {
       var count : scala.Long = 0
@@ -158,23 +153,21 @@ object GetHotSpots
       }
       (count, weight)
     }
-    
     // Calculation of Formula parameters
     val horizontalPartitions = Math.ceil((boundaryEnvelope.getMaxX - boundaryEnvelope.getMinX) / 0.01).toInt
-		val verticalPartitions = Math.ceil((boundaryEnvelope.getMaxY - boundaryEnvelope.getMinY) / 0.01).toInt
-		val numCells = horizontalPartitions * verticalPartitions * 31
-    val intermean = (joinResults.map(x => x._2).reduce((x, y) =>  x + y)).toDouble 
-    println("Total joinResults reduced value " + intermean)
+  	val verticalPartitions = Math.ceil((boundaryEnvelope.getMaxY - boundaryEnvelope.getMinY) / 0.01).toInt
+  	val numCells = horizontalPartitions * verticalPartitions * 31
+    val intermean = (cubeAttributeRDD.map(x => x._2).reduce((x, y) =>  x + y)).toDouble 
     val mean = intermean / numCells
-    val standardDeviation = Math.sqrt(((joinResults.map(x => x._2 * x._2).reduce((x, y) => x + y)).toDouble / numCells) - (mean * mean))
+    val standardDeviation = Math.sqrt(((cubeAttributeRDD.map(x => x._2 * x._2).reduce((x, y) => x + y)).toDouble / numCells) - (mean * mean))
     
     println("mean : " + mean + " standard deviation : " +  standardDeviation + " num cells " + numCells + " hp " + horizontalPartitions + " vp " + verticalPartitions)
     val meanBC = sc.broadcast(mean)
     val standardDeviationBC = sc.broadcast(standardDeviation)
     val numCellsBC = sc.broadcast(numCells)
-
+  
     // Get the square list
-    val squaresRDD = sc.parallelize(rectangleGrids._2.asScala)
+    val squaresRDD = sc.parallelize(rectangleGrids.asScala)
     var finalResults = sc.emptyRDD[((scala.Double, scala.Double, scala.Int), scala.Double)]
     for (i <- 1 to 31)
     {
@@ -188,7 +181,6 @@ object GetHotSpots
                                     ((x._1, x._2, i), finalValue)
                                   })
     }
-    finalResults.sortBy(_._2, false).take(50).foreach(x => println( x._1._2 + ", " + x._1._1 + ", "+ x._1._3 + ", " + x._2))
+    finalResults.sortBy(_._2, false).take(50).foreach(x => println( x._1._2 + ", " + x._1._1 + ", "+ x._1._3 + ", " + x._2)) 
   }
-  
 }
